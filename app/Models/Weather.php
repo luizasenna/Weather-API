3ee3e84db253aa041a1bbaca1d4f033ec8bef23a
Luiza Senna <eu@luizasenna.com>
2022-05-03 12:47:06 -0300

    models and migrations

---
+++ app/Models/Weather.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Weather extends Model
+{
+    use HasFactory;
+
+    public $timestamps = true;
+    protected $fillable = ['city_id', 'main', 'description','temp', 'feels_like',
+        'temp_min', 'temp_max', 'pressure', 'humidity'];
+    use SoftDeletes;
+
+
+    protected $dates = ['deleted_at'];
+
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+}
---
+++ database/migrations/2022_05_03_145619_create-weather.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer("city_id");
+            $table->string("main");
+            $table->string("description");
+            $table->string("temp");
+            $table->string("feels_like");
+            $table->string("temp_min");
+            $table->string("temp_max");
+            $table->string("pressure");
+            $table->string("humidity");
+
+            $table->foreign('city_id')->references('id')->on('cities');
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather');
+    }
+};
